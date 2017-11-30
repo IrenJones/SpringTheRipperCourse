4ffa03c79b8a35508b72f15ad76abc3e4e611409
Iren <nor.iren@yandex.ru>
2017-11-30 16:33:42 +0300

    add init method and several phases

--- src/main/java/quoters/TerminatorQuoter.java
+++ src/main/java/quoters/TerminatorQuoter.java
@@ -1,12 +1,21 @@
 package quoters;
 
+import javax.annotation.PostConstruct;
+
 public class TerminatorQuoter implements Quoter {
     @InjectRandomInt (min = 2, max = 7)
     private int repeat;
 
     private String message;
 
+    @PostConstruct
+    public void init(){
+        System.out.println("Phase 2");
+        System.out.println(repeat);
+    }
+
     public TerminatorQuoter() {
+        System.out.println("Phase 1");
     }
 
     public void setMessage(String message) {
